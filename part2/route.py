1a6f9731f02a7f4c5c9cc59456df6df45fa39a82
Cody Harris <harrcody@iu.edu>
2020-09-30 21:29:48 -0400

    Added Cost Functions

--- part2/route.py
+++ part2/route.py
@@ -29,20 +29,62 @@ def load_data(file_name):
     return data_list
   
 #segments cost function
-def segments_cost():
-    return
+def segments_cost(route):
+    return (len(route) - 1)
 
 # distance cost function
-def distance_cost():
-    return
+def distance_cost(route):
+    dist = 0
+    if len(route) == 2:
+        for i in range(len(road_segs)):
+            if road_segs[i][0] == route[0] and road_segs[i][1] == route[1] or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                exit
+        return round(dist,5)
+    else:
+        for i in range(len(road_segs)):
+            if road_segs[i][0] == route[0] and road_segs[i][1] == route[1] or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                exit
+        return round(dist  + distance_cost(route[1:]),5)
 
 # time cost function
-def time_cost():
-    return
+def time_cost(route):
+    dist = 0
+    speed = 0
+    if len(route) == 2:
+        for i in range(len(road_segs)):
+            if road_segs[i][0] == route[0] and road_segs[i][1] == route[1] or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                speed = int(road_segs[i][3]) + 5
+                exit
+        return round(dist / speed, 5)
+    else:
+        for i in range(len(road_segs)):
+            if road_segs[i][0] == route[0] and road_segs[i][1] == route[1] or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                speed = int(road_segs[i][3]) + 5
+                exit
+        return round(dist / speed + time_cost(route[1:]),5)
 
 # cycling cost function
-def cycling_cost():
-    return
+def cycling_cost(route):
+    dist = 0
+    speed = 0
+    if len(route) == 2:
+        for i in range(len(road_segs)):
+            if (road_segs[i][0] == route[0] and road_segs[i][1] == route[1]) or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                speed = int(road_segs[i][3])
+                exit
+        return round(speed * 0.000001 * dist, 5)
+    else:
+        for i in range(len(road_segs)):
+            if road_segs[i][0] == route[0] and road_segs[i][1] == route[1] or (road_segs[i][0] == route[1] and road_segs[i][1] == route[0]):
+                dist = int(road_segs[i][2])
+                speed = int(road_segs[i][3])
+                exit
+        return round(speed * 0.000001 * dist + cycling_cost(route[1:]),5)
 
 # graph preprocessing for missing cities in long/lat
 def missing_cities():
@@ -50,10 +92,11 @@ def missing_cities():
 
 # distance between two points, using long/lat
 def euclid_dist():
-    return
+    return 0
 
 # return a list of possible successor states
 def successors(state, edge):
+    print('State', state)
     match_first = [city[1:4] for city in edge if city[0] in state]
     match_second = [tuple([city[0], city[2], city[3]]) for city in edge if city[1] in state]
     return match_first + match_second
@@ -67,10 +110,7 @@ def solve(route_params):
     fringe = PriorityQueue()
     #Fringe is (priority, (current city, route, cost))
     fringe.put((0, (route_params[0], [], 0)))
-    visited = []
-    road_segs = load_data('road-segments.txt')
-    gps = load_data('city-gps.txt')
-    
+    visited = []  
     while not fringe.empty():
         priority, data = fringe.get()
         state, route_so_far, cost = data
@@ -80,7 +120,8 @@ def solve(route_params):
                 if is_goal(succ, route_params[1]):
                     return(route_so_far.append(succ))
                     # This is where we can try out different heuristics
-                route_so_far = route_so_far.append(succ)
+                route_so_far.append(succ)
+
                 cost_so_far = calc_cost(route_so_far, route_params[2])
                 fringe.put((cost_so_far + euclid_dist(succ), (succ, route_so_far, cost_so_far)))
                 
@@ -95,7 +136,8 @@ if __name__ == "__main__":
         raise(Exception("Error: Cost function should be one of: segments, distance, time, cycling, statetour"))
         
     start_state = tuple(sys.argv[1:])   
-
+    road_segs = load_data('road-segments.txt')
+    gps_data = load_data('city-gps.txt')
     print("Solving...")
     route = solve(tuple(start_state))
     
