7b158b22b081961800aabba06e9f55260d7ce9a8
Cody Harris <harrcody@iu.edu>
2020-10-02 19:58:58 -0400

    Update to hueristic

--- part2/route.py
+++ part2/route.py
@@ -116,7 +116,7 @@ def get_distance_apart(start, end):
         end_lat_long = [coords for coords in gps_data if coords[0] in end]
         end_lat = float(end_lat_long[0][1])
         end_long = float(end_lat_long[0][2])
-        sp_dist = spherical_dist_calc(start_lat, start_long, end_lat, end_long) - 5
+        sp_dist = spherical_dist_calc(start_lat, start_long, end_lat, end_long)
         if sp_dist > max_segment_length:
             return shortest_dist
         else:
@@ -170,8 +170,7 @@ def solve(route_params):
             for succ in successors(curr_city):
                 cost_so_far = calc_cost([curr_city, succ[0]], cost_function) + cost
                 fringe.put((cost_so_far + hueristic(succ[0], end_city, route_so_far + [succ[0]], cost_function), (succ[0], route_so_far + [succ[0]], cost_so_far)))
-                #fringe.put((cost_so_far, (succ[0], route_so_far + [succ[0]], cost_so_far)))
-        return False
+    return False
 
 
 if __name__ == "__main__":
